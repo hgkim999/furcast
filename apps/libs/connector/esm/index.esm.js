f72f754e7a591a320ad8f71960e73baaac112aac
Samuel Kim <samuel@fullcontext.ai>
2024-12-08 13:57:19 -0800

    init React 19 firebase

---
+++ apps/libs/connector/esm/index.esm.js
@@ -0,0 +1,8 @@
+import { getDataConnect, validateArgs } from 'firebase/data-connect';
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'furcast-server',
+  location: 'us-west1'
+};
+
---
+++ libs/core/tsup.config.ts
@@ -0,0 +1,14 @@
+import {
+  defineConfig,
+  Options,
+} from 'tsup';
+
+export default defineConfig((options: Options) => ({
+  entry: {
+    index: "src/index.ts",
+  },
+  clean: true,
+  format: ["cjs", "esm"],
+  dts: true,
+  ...options,
+}));
